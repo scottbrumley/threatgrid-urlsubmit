243d99cc4cb85f7bb9bc9cdf29d40e9ca6d9c57f
sbrumley <sbrumley@wireless-town.com>
2018-12-04 20:17:37 -0500

    Initial commit

---
+++ threatgrid-urlsubmit.js
@@ -0,0 +1,29 @@
+module.exports = function(RED) {
+    function URLSubmitNode(config) {
+        RED.nodes.createNode(this,config);
+        this.apikey = config.apikey;
+        this.url = config.url;
+        var node = this;
+        node.on('input', function(msg) {
+            submitURL = encodeURIComponent(msg.url);
+            urlStr = "https://panacea.threatgrid.com/api/v2/search/submissions?q=" + submitURL + "&api_key=" + node.apikey;
+
+            //msg.payload = urlStr;
+            //node.send(msg);
+
+            request = require('request');
+            request(urlStr, function(err, res, body) {
+                if (err) {
+                    msg.payload = err;
+                    node.send(msg);
+                } else {
+                    msg.payload = body;
+                    node.send(msg);
+                }
+            });
+
+
+        });
+    }
+    RED.nodes.registerType("threatgrid-urlsubmit",URLSubmitNode);
+}
