8490d43336a1bb0f077d041f44d9524899dbf840
sbrumley <sbrumley@wireless-town.com>
2019-01-17 13:43:57 -0500

    Added configuration node to existing files

--- threatgrid-urlsubmit.js
+++ threatgrid-urlsubmit.js
@@ -1,20 +1,33 @@
 module.exports = function(RED) {
     function URLSubmitNode(config) {
         RED.nodes.createNode(this,config);
-        this.apikey = config.apikey;
-        this.url = config.url;
         var node = this;
         node.on('input', function(msg) {
             submitURL = encodeURIComponent(msg.url);
-            urlStr = "https://panacea.threatgrid.com/api/v2/search/submissions?q=" + submitURL + "&api_key=" + msg.apikey;
-
-            //msg.payload = urlStr;
             //node.send(msg);
 
             request = require('request');
-            request(urlStr, function(err, res, body) {
-                if (err) {
-                    msg.payload = err;
+
+            var options = { method: 'POST',
+            url: 'https://panacea.threatgrid.com/api/v2/samples',
+            qs: { api_key: 'g1gkukoavkk76g9fjj0qcbkjfg' },
+            headers:
+             {'Postman-Token': '10eb7bac-26c6-4f9f-953b-7cb085896b8a',
+              'Cache-Control': 'no-cache',
+              'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
+            formData:
+             {sample:
+              {value: '[InternetShortcut]\r\n\URL=' + submitURL + '\r\n',
+               options:
+               {filename: 'sample',
+                contentType: null } } } };
+
+            request(options, function(error, response, body) {
+                console.log('error:', error); // Print the error if one occurred
+                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                console.log('body:', body); // Print the HTML for the Google homepage.
+                if (error) {
+                    msg.payload = error;
                     node.send(msg);
                 } else {
                     msg.payload = body;
@@ -25,5 +38,12 @@ module.exports = function(RED) {
 
         });
     }
+    function ThreatGrid(n) {
+        RED.nodes.createNode(this,n);
+        this.apikey = n.apikey;
+        this.scanwait = n.scanwait;
+    }
     RED.nodes.registerType("threatgrid-urlsubmit",URLSubmitNode);
+    RED.nodes.registerType("threat-grid",ThreatGrid);
+
 }
